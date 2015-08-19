4ac12417a23e112bbdd7741aef8b5aa7ac27b999
vikrum <vsnijjar+github@gmail.com>
2015-08-18 23:26:44 -0700

    Added orange and brown.

--- js/colors.js
+++ js/colors.js
@@ -17,8 +17,10 @@ KiddoPaint.Colors.All = {
   colorlblue: 'rgb(0, 0, 255)',
   colordpurple: 'rgb(128, 0, 128)',
   colorlpurple: 'rgb(255, 0, 255)',
-  colorlbrown: 'rgb(187, 145, 95)',
-  colordbrown: 'rgb(136, 104, 67)'
+  colorlbrown: 'rgb(136, 104, 67)',
+  colordbrown: 'rgb(73, 61, 38)',
+  colordorange: 'rgb(225, 135, 0)',
+  colorlorange: 'rgb(255, 195, 30)'
 }
 
 KiddoPaint.Colors.nextColor = function() {

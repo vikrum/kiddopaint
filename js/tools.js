97a4f293a466e8310ae701a0e2fa6354535a8be7
vikrum <vsnijjar+github@gmail.com>
2015-09-04 22:44:22 -0700

    Properly preview magnify tool.

--- js/tools.js
+++ js/tools.js
@@ -491,13 +491,14 @@ KiddoPaint.Tools.Toolbox.Magnify = function() {
 
 	this.mousedown = function (ev) {
 		tool.isDown = true;
+		tool.mousemove(ev);
 	};
 
 	this.mousemove = function (ev) {
-		KiddoPaint.Display.clearTmp();
 		var target = KiddoPaint.Display.main_context.getImageData(ev._x - tool.size, ev._y - tool.size, 2 * tool.size, 2 * tool.size);
 		var scaled = scaleImageData(target, tool.scale);
-		KiddoPaint.Display.context.putImageData(scaled, ev._x - (tool.scale * tool.size), ev._y - (tool.scale * tool.size));
+		var ctx = tool.isDown ? KiddoPaint.Display.context : KiddoPaint.Display.previewContext;
+		ctx.putImageData(scaled, ev._x - (tool.scale * tool.size), ev._y - (tool.scale * tool.size));
 	};
 
 	this.mouseup = function (ev) {

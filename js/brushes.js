bd4d01afe98978b2480154776b7cd0a8a658772a
vikrum <vsnijjar+github@gmail.com>
2015-09-05 21:46:46 -0700

    Properly flip fill state of circles brush.

--- js/brushes.js
+++ js/brushes.js
@@ -1,27 +1,30 @@
-KiddoPaint.Brushes.Circles = function(color1, color2, alwaysFill) {
-	color1 = color1 || 'black';
-	color2 = color2 || color1;
-	alwaysFill = alwaysFill || false;
-	var size = 20;
-
-	var canvasBrush = document.createElement('canvas');
-	canvasBrush.width = size * 2;
-	canvasBrush.height = size * 2;
-	var contextBrush = canvasBrush.getContext('2d');
-
-	contextBrush.beginPath();
-	contextBrush.arc(size, size, 10, 0, 2 * Math.PI);
-	if(alwaysFill || KiddoPaint.Display.step % 2 == 0) {
-	  contextBrush.fillStyle = color1;
-	  contextBrush.fill();
-	}
-	contextBrush.lineWidth = 2;
-	contextBrush.strokeStyle = color2;
-	contextBrush.stroke();
-	contextBrush.closePath();
+KiddoPaint.Brushes.Circles = function() {
+	var flip = 0;
+	return function(color1, color2, alwaysFill) {
+		color1 = color1 || 'black';
+		color2 = color2 || color1;
+		alwaysFill = alwaysFill || false;
+		var size = 20;
 	
-	return {brush: canvasBrush, offset: size};
-}
+		var canvasBrush = document.createElement('canvas');
+		canvasBrush.width = size * 2;
+		canvasBrush.height = size * 2;
+		var contextBrush = canvasBrush.getContext('2d');
+	
+		contextBrush.beginPath();
+		contextBrush.arc(size, size, 10, 0, 2 * Math.PI);
+		if(alwaysFill || flip % 2 == 0) {
+		  contextBrush.fillStyle = color1;
+		  contextBrush.fill();
+		}
+		contextBrush.lineWidth = 2;
+		contextBrush.strokeStyle = color2;
+		contextBrush.stroke();
+		contextBrush.closePath();
+		flip++;
+		
+		return {brush: canvasBrush, offset: size};
+}}();
 
 KiddoPaint.Brushes.RCircles = function() {
 	var color1 = KiddoPaint.Colors.randomColor(); 

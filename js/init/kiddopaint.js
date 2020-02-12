7c3d92233c73d4d2eb2f2f852adb36d94b6862c9
Vikrum Nijjar <vsnijjar@gmail.com>
2020-02-11 20:07:57 -0800

    Minor updates.
    - Disable right click for small fingers.
    - Color modes added to other tools.

--- js/init/kiddopaint.js
+++ js/init/kiddopaint.js
@@ -14,6 +14,7 @@ KiddoPaint.Cache = {};
 KiddoPaint.Alphabet = {};
 
 function init_kiddo_paint() {
+  document.addEventListener("contextmenu", function(e){ e.preventDefault(); }, false);
   var canvas = document.getElementById('kiddopaint');
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
@@ -482,7 +483,7 @@ function init_brush_subtoolbar() {
   document.getElementById('br9').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 40; KiddoPaint.Tools.PlainBrush.texture = function() { return KiddoPaint.Brushes.Pies(KiddoPaint.Current.color) }; });
   document.getElementById('br10').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 1; KiddoPaint.Tools.PlainBrush.texture = function(step) { return KiddoPaint.Brushes.Concentric(KiddoPaint.Current.color, step) }; });
   document.getElementById('br11').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 1; KiddoPaint.Tools.PlainBrush.texture = function(step) { return KiddoPaint.Brushes.Twirly(KiddoPaint.Current.modified ? KiddoPaint.Colors.nextColor() : KiddoPaint.Current.color, step) }; });
-  document.getElementById('br12').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 1; KiddoPaint.Tools.PlainBrush.texture = function(step) { return KiddoPaint.Current.modifiedAlt ? KiddoPaint.Brushes.RotatingPentagon(KiddoPaint.Current.color, step) : KiddoPaint.Brushes.FollowingSine(KiddoPaint.Current.color, step) }; });
+  document.getElementById('br12').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 1; KiddoPaint.Tools.PlainBrush.texture = function(step) { return KiddoPaint.Current.modifiedAlt ? KiddoPaint.Brushes.RotatingPentagon(KiddoPaint.Current.modified ? KiddoPaint.Colors.nextColor() : KiddoPaint.Current.color, step) : KiddoPaint.Brushes.FollowingSine(KiddoPaint.Current.modified ? KiddoPaint.Colors.nextColor() : KiddoPaint.Current.color, step) }; });
   document.getElementById('br13').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.PlainBrush; KiddoPaint.Tools.PlainBrush.spacing = 1; KiddoPaint.Tools.PlainBrush.texture = function(step) { return KiddoPaint.Brushes.Rose(KiddoPaint.Current.color, step) }; });
   document.getElementById('br14').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.Guilloche; });
   document.getElementById('br15').addEventListener('mousedown', function() { KiddoPaint.Current.tool = KiddoPaint.Tools.Contours; });

3bcd5252a8597368c6a13b4cb1fc25192d4ced9e
Vikrum Nijjar <vsnijjar@gmail.com>
2020-09-07 16:48:43 -0700

    Fix jagged pencil tool.

--- js/attic/tools.js
+++ js/attic/tools.js
@@ -7,14 +7,16 @@ KiddoPaint.Tools.Toolbox.Pencil = function() {
 	this.mousedown = function (ev) {
 		tool.isDown = true;
 		KiddoPaint.Display.context.beginPath();
+		KiddoPaint.Display.context.strokeStyle = tool.texture();
+		KiddoPaint.Display.context.lineWidth = tool.size;
+		KiddoPaint.Display.context.lineCap = 'round';
+		KiddoPaint.Display.context.lineJoin = 'round';
 		KiddoPaint.Display.context.moveTo(ev._x, ev._y);
 	};
 
 	this.mousemove = function (ev) {
 		if (tool.isDown) {
 			KiddoPaint.Display.context.lineTo(ev._x, ev._y);
-			KiddoPaint.Display.context.strokeStyle = tool.texture();
-			KiddoPaint.Display.context.lineWidth = tool.size;
 			KiddoPaint.Display.context.stroke();
 		}
 	};

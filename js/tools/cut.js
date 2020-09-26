2097c847205c24c2b5c337ebe6c5f434b0cb6933
Vikrum Nijjar <vsnijjar@gmail.com>
2020-09-26 15:51:16 -0700

    Add support to change size of cut tool.

--- js/tools/cut.js
+++ js/tools/cut.js
@@ -2,7 +2,10 @@ KiddoPaint.Tools.Toolbox.Cut = function() {
     var tool = this;
     this.isDown = false;
     this.size = function() {
-        return 50 * KiddoPaint.Current.scaling * KiddoPaint.Current.multiplier;
+        return {
+            x: 50 * KiddoPaint.Current.scaling * KiddoPaint.Current.multiplier * ((KiddoPaint.Current.modifiedCtrlRange + 100) / 100),
+            y: 50 * KiddoPaint.Current.scaling * KiddoPaint.Current.multiplier * ((KiddoPaint.Current.modifiedAltRange + 100) / 100)
+        }
     }
     this.stomp = true;
     this.selectedData;
@@ -16,14 +19,19 @@ KiddoPaint.Tools.Toolbox.Cut = function() {
         tool.isDown = true;
         tool.x = ev._x;
         tool.y = ev._y;
+        sizex = tool.size().x;
+        sizey = tool.size().y;
         if (!KiddoPaint.Current.modifiedToggle) {
             // save previous capture to reuse; click and drag to reinstantiate
-            tool.selectedData = KiddoPaint.Display.main_context.getImageData(ev._x - tool.size(), ev._y - tool.size(), 2 * tool.size(), 2 * tool.size());
+            tool.selectedData = KiddoPaint.Display.main_context.getImageData(ev._x - sizex, ev._y - sizey, 2 * sizex, 2 * sizey);
         }
         tool.mousemove(ev);
     };
 
     this.mousemove = function(ev) {
+        sizex = tool.size().x;
+        sizey = tool.size().y;
+
         if (tool.stomp) {
             KiddoPaint.Display.clearTmp();
         }
@@ -31,28 +39,30 @@ KiddoPaint.Tools.Toolbox.Cut = function() {
         if (tool.isDown || (KiddoPaint.Current.modifiedToggle && tool.selectedData)) {
             if (KiddoPaint.Current.modifiedMeta) { // preview what a cut will look like
                 KiddoPaint.Display.previewContext.fillStyle = 'white';
-                KiddoPaint.Display.previewContext.fillRect(tool.x - tool.size(), tool.y - tool.size(), 2 * tool.size(), 2 * tool.size());
+                KiddoPaint.Display.previewContext.fillRect(tool.x - sizex, tool.y - sizey, 2 * sizex, 2 * sizey);
             }
-            KiddoPaint.Display.previewContext.putImageData(tool.selectedData, ev._x - tool.size(), ev._y - tool.size());
+            KiddoPaint.Display.previewContext.putImageData(tool.selectedData, ev._x - sizex, ev._y - sizey);
         } else {
             KiddoPaint.Display.previewContext.strokeStyle = 'grey';
             KiddoPaint.Display.previewContext.lineWidth = 0.5;
             KiddoPaint.Display.previewContext.setLineDash((KiddoPaint.Display.step % 2) ? [4] : [2]);
-            KiddoPaint.Display.previewContext.strokeRect(ev._x - tool.size(), ev._y - tool.size(), 2 * tool.size(), 2 * tool.size());
+            KiddoPaint.Display.previewContext.strokeRect(ev._x - sizex, ev._y - sizey, 2 * sizex, 2 * sizey);
         }
     };
 
     this.mouseup = function(ev) {
         if (tool.isDown) {
+            sizex = tool.size().x;
+            sizey = tool.size().y;
             tool.isDown = false;
-            KiddoPaint.Display.context.putImageData(tool.selectedData, ev._x - tool.size(), ev._y - tool.size());
+            KiddoPaint.Display.context.putImageData(tool.selectedData, ev._x - sizex, ev._y - sizey);
 
             // Since we're manipulating main_context and we need to do it between undo() and cleartmp(), we dupe
             // and peek into abstraction here
             // KiddoPaint.Display.saveMain();
             KiddoPaint.Display.saveUndo();
             if (KiddoPaint.Current.modifiedMeta) { // actually do the cut; and before placing the new pixels
-                KiddoPaint.Display.main_context.clearRect(tool.x - tool.size(), tool.y - tool.size(), 2 * tool.size(), 2 * tool.size());
+                KiddoPaint.Display.main_context.clearRect(tool.x - sizex, tool.y - sizey, 2 * sizex, 2 * sizey);
             }
             KiddoPaint.Display.main_context.drawImage(KiddoPaint.Display.canvas, 0, 0);
             KiddoPaint.Display.clearTmp();

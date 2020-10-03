451beb26ac55234f875859b202fbc155762d9a7a
Vikrum Nijjar <vsnijjar@gmail.com>
2020-10-02 23:33:47 -0700

    Fix truncating prints and re-support colorization.

--- js/builders/prints.js
+++ js/builders/prints.js
@@ -3,19 +3,21 @@ KiddoPaint.Builders.Prints = function(color1, print, angle) {
     angle = angle || 0;
 
     var canvasBrush = document.createElement('canvas');
-    canvasBrush.width = 43;
-    canvasBrush.height = 43;
+    canvasBrush.width = 100;
+    canvasBrush.height = 100;
     var contextBrush = canvasBrush.getContext('2d');
 
-    contextBrush.beginPath();
-
-    contextBrush.translate(21, 21);
+    contextBrush.save();
+    contextBrush.translate(50, 50);
     contextBrush.rotate(angle);
-    contextBrush.translate(-15.5, -15.5);
-
+    contextBrush.translate(-25, 0);
     contextBrush.font = '36px sans-serif';
-    contextBrush.fillStyle = KiddoPaint.Textures.Solid(color1);
-    contextBrush.fillText(print, 3, 30);
+    contextBrush.fillText(print, 0, 0);
+    contextBrush.restore();
 
-    return canvasBrush;
+    contextBrush.globalCompositeOperation = 'source-atop';
+    contextBrush.fillStyle = color1;
+    contextBrush.fillRect(0, 0, canvasBrush.width, canvasBrush.height);
+
+    return trimCanvas(canvasBrush);
 }

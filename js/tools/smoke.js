0ec67f04fbe24a45412e3139cfbeabd11282da15
Vikrum Nijjar <vsnijjar@gmail.com>
2020-10-25 18:08:33 -0700

    Save and restore context around smoke.

--- js/tools/smoke.js
+++ js/tools/smoke.js
@@ -4,17 +4,22 @@ KiddoPaint.Tools.Toolbox.Smoke = function() {
     this.party = {};
 
     this.mousedown = function(ev) {
-        var smokeColor = color2json(KiddoPaint.Current.color);
         tool.isDown = true;
+        KiddoPaint.Display.context.save();
+        var smokeColor = color2json(KiddoPaint.Current.color);
         tool.party = SmokeMachine(KiddoPaint.Display.context, [smokeColor.r, smokeColor.g, smokeColor.b]); // ;
         tool.party.start();
         setTimeout(function() {
-            tool.party.addSmoke(ev._x, ev._y, 128);
+            tool.party.addSmoke(ev._x, ev._y, 64);
             tool.party.step(10);
         }, 100);
     };
 
-    this.mousemove = function(ev) {};
+    this.mousemove = function(ev) {
+        if (tool.isDown) {
+            tool.party.addSmoke(ev._x, ev._y, 4);
+        }
+    };
 
     this.mouseup = function(ev) {
         if (tool.isDown) {
@@ -23,6 +28,7 @@ KiddoPaint.Tools.Toolbox.Smoke = function() {
                 tool.party.stop();
                 KiddoPaint.Display.saveMain();
                 tool.party = {};
+                KiddoPaint.Display.context.restore();
             }, 100);
         }
     };
